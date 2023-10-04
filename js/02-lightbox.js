2bcf1de7f9c8c5e7577c8cd01655f4251bf6a017
pavloit <dba.devop@gmail.com>
2023-10-04 12:45:30 +0300

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.map(({ description, original, preview }) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`).join("");
+
+gallery.innerHTML = markup;
+gallery.addEventListener('click', handlerClick)
+function handlerClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return
+    }
+    return new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+    });
+}
